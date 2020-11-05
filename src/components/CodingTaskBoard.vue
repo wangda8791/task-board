8b19cceb685359210977fe1b01de57a1d9495abe
Wang Da <wangda8791@163.com>
2020-11-06 05:55:37 +1000

    Add CodingTaskBoard empty component

---
+++ src/components/CodingTaskBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Coding Task Board</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CodingTaskBoard",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
